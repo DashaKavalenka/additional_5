9e951d03fe45b22e34b8578cd92e73c1dd435e3c
DashaKavalenka <mymailDasha@gmail.com>
2018-02-28 21:19:32 +0300

    Implement function to check brackets sequence (second way)

--- src/index.js
+++ src/index.js
@@ -1,39 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-    // your solution
-    var openChars = [],
-        closeChars = [],
-        openBracket,
-        closeBracket,
-        openCharIndex,
-        closeCharIndex,
-        stack = [],
-        stringArray = str.split('');
-    if (stringArray.length % 2 !== 0) {
-       return false;
-    }
-    for (var i = 0, len = bracketsConfig.length; i<len; i++){
-        openBracket = bracketsConfig[i][0];
-        closeBracket = bracketsConfig[i][1];
-        if (openBracket !== closeBracket) {
-            openChars.push(openBracket);
-            closeChars.push(closeBracket);
-        }
-    } //Разбиваем второй параметр на 2 масива( открывающие, закрывающие) сохраняя соответствие индексов открывающих закрывающим
-    for (var i = 0, len = stringArray.length; i < len; i++) {
-        openCharIndex = openChars.indexOf(stringArray[i]);
-        if (openCharIndex !== -1) {
-            stack.push(openCharIndex);
-            continue;
-        }
+    var result,
+        lengthOfString,
+        openSign,
+        closeSign,
+        maxCounter = 300,
+        counter = 0;
+    do {
+        for (var i = 0; i < bracketsConfig.length; i++) {
+            counter = counter + 1;
+            openSign = bracketsConfig[i][0];
+            closeSign = bracketsConfig[i][1];
 
-        closeCharIndex = closeChars.indexOf(stringArray[i]);
-        if (closeCharIndex !== -1) {
-            openCharIndex = stack.pop();
-            if (closeCharIndex !== openCharIndex) {
-                return false;
+            for (var k = 0; k < str.length; k++) {
+                if (str.charAt(k) == openSign && str.charAt(k + 1) == closeSign) {
+                    str = str.replace(openSign + closeSign, '');
+                }
             }
+            lengthOfString = str.length;
         }
-    }
-    return stack.length === 0;
+        if (counter > maxCounter) {
+            break;
+        }
 
+    } while (lengthOfString !== 0);
+    if (lengthOfString === 0) {
+        result = true;
+    } else {
+        result = false;
+    }
+    return result;
 }
